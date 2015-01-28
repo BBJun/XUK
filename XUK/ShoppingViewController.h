a4d1a6e05f4bf25733dde4d812afe5d404d252d7
Your Name <you@example.com>
2015-01-28 12:21:41 +0800

    new viewController

---
+++ XUK/ShoppingViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShoppingViewController.h
+//  XUK
+//
+//  Created by 123 on 15-1-28.
+//  Copyright (c) 2015年 andy. All rights reserved.
+//
+
+#import "MBaseViewController.h"
+
+@interface ShoppingViewController : MBaseViewController
+
+@end
